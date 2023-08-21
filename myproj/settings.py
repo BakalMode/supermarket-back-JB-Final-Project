f0b12581fac1ddc3bd462c6ade0b04c9195a4298
Uriel.shimoni <119458152+BakalMode@users.noreply.github.com>
2023-08-22 02:14:03 +0300

    setting changes

--- myproj/settings.py
+++ myproj/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv('../.env')
+load_dotenv('/etc/secrets/.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
