e9532d59475eaed2ea2d447782af1c8eede18f34
Uriel.shimoni <119458152+BakalMode@users.noreply.github.com>
2023-08-26 19:19:02 +0300

    added ENV for production

--- myproj/settings.py
+++ myproj/settings.py
@@ -4,10 +4,10 @@ import os
 from dotenv import load_dotenv
 
 
-# if(os.environ['ENV']=='production'):
-#     load_dotenv('/etc/secrets/.env')
-# else: load_dotenv()
-load_dotenv()
+if(os.environ['ENV']=='production'):
+    load_dotenv('/etc/secrets/.env')
+else: load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
